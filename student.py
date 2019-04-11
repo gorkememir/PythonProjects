f109ac3847f89f85cb8e33447c831e76d5a6801d
gorkem <gorkememir@gmail.com>
2019-04-11 14:33:08 -0400

    Student for loop. Key, value pairs

---
+++ student.py
@@ -0,0 +1,8 @@
+students = {
+    "male": ["Tom", "Charlie", "Harry", "Frank"],
+    "female":["Sarah", "Huda", "Samantha", "Emily", "Elizabeth"]
+    }
+
+for anahtar in students.keys():
+    for isim in students[anahtar]:
+        print(anahtar, isim)
